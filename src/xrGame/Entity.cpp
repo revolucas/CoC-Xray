c63698bb5bbbb818ee0dd7bfe2bca8adeaa25f8d
revolucas <revolucas@gmail.com>
2017-03-05 19:01:03 -0500

    = fix actor before death

--- src/xrGame/Entity.cpp
+++ src/xrGame/Entity.cpp
@@ -276,11 +276,13 @@ void CEntity::KillEntity(u16 whoID, BOOL bypass_actor_check /*AVO: added for act
         }
 #endif
     }
+	/* Alundaio: Should not matter who kills self. CScripGameObject::Kill sets self as killer if nil passed
     else
     {
         if (m_killer_id != ALife::_OBJECT_ID(-1))
             return;
     }
+	*/
 
     m_killer_id = whoID;
 
