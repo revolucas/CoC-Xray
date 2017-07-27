01e05b079febaa5006d1e2406aad94b0658b2e56
pi7676\qawsed <qweasdd136963@gmail.com>
2017-07-28 00:23:22 +0300

    Исправление неправильной загрузки памяти нпс

--- src/xrGame/sound_memory_manager.cpp
+++ src/xrGame/sound_memory_manager.cpp
@@ -454,12 +454,12 @@ void CSoundMemoryManager::load	(IReader &packet)
 #ifdef USE_LEVEL_TIME
 		VERIFY						(Device.dwTimeGlobal >= object.m_level_time);
 		object.m_level_time			= packet.r_u32();
-		object.m_level_time			+= Device.dwTimeGlobal;
+		object.m_level_time			= Device.dwTimeGlobal - object.m_level_time;
 #endif // USE_LEVEL_TIME
 #ifdef USE_LAST_LEVEL_TIME
 		VERIFY						(Device.dwTimeGlobal >= object.m_last_level_time);
 		object.m_last_level_time	= packet.r_u32();
-		object.m_last_level_time	+= Device.dwTimeGlobal;
+		object.m_last_level_time	= Device.dwTimeGlobal - object.m_last_level_time;
 #endif // USE_LAST_LEVEL_TIME
 #ifdef USE_FIRST_LEVEL_TIME
 		VERIFY						(Device.dwTimeGlobal >= (*I).m_first_level_time);
