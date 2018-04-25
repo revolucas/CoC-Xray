f2c790ebef9374bcfcbe902c2cd18b1321e9f5b1
Balathruin <balathruin@gmail.com>
2018-04-25 13:35:32 +0200

    ‍
    ‍

--- src/xrGame/purchase_list.cpp
+++ src/xrGame/purchase_list.cpp
@@ -15,17 +15,17 @@
 
 static float min_deficit_factor = .3f;
 
-void CPurchaseList::process(CInifile &ini_file, LPCSTR section, CInventoryOwner &owner)
+void CPurchaseList::process	(CInifile &ini_file, LPCSTR section, CInventoryOwner &owner)
 {
 	owner.sell_useless_items();
 
-	m_deficits.clear();
+	m_deficits.clear		();
 
 	const CGameObject		&game_object = smart_cast<const CGameObject &>(owner);
 	CInifile::Sect			&S = ini_file.r_section(section);
 	CInifile::SectCIt		I = S.Data.begin();
 	CInifile::SectCIt		E = S.Data.end();
-	for (; I != E; ++I) {
+	for ( ; I != E; ++I) {
 		if (!(*I).second.size())
 			continue;
 
@@ -39,40 +39,40 @@ void CPurchaseList::process(CInifile &ini_file, LPCSTR section, CInventoryOwner 
 		LPCSTR count = _GetItem(*(*I).second, 0, temp0);
 		LPCSTR prob = _GetItemCount(*(*I).second) >= 2 ? _GetItem(*(*I).second, 1, temp1) : "1.0f";
 
-		process(
+		process				(
 			game_object,
 			(*I).first,
 			atoi(count),
 			(float)atof(prob)
-			);
+		);
 	}
 }
 
-void CPurchaseList::process(const CGameObject &owner, const shared_str &name, const u32 &count, const float &probability)
+void CPurchaseList::process	(const CGameObject &owner, const shared_str &name, const u32 &count, const float &probability)
 {
-	VERIFY3(count, "Invalid count for section in the purchase list", *name);
-	VERIFY3(!fis_zero(probability, EPS_S), "Invalid probability for section in the purchase list", *name);
+	VERIFY3					(count,"Invalid count for section in the purchase list",*name);
+	VERIFY3					(!fis_zero(probability,EPS_S),"Invalid probability for section in the purchase list",*name);
 
 	const Fvector			&position = owner.Position();
 	const u32				&level_vertex_id = owner.ai_location().level_vertex_id();
 	const ALife::_OBJECT_ID	&id = owner.ID();
-	CRandom					random((u32)(CPU::QPC() & u32(-1)));
-	for (u32 i = 0, j = 0; i<count; ++i) {
-		if (random.randF() > probability)
+
+	for (u32 i=0, j=0; i<count; ++i) {
+		if (::Random.randF(0.f, 1.f) > probability)
 			continue;
 
 		++j;
-		Level().spawn_item(*name, position, level_vertex_id, id, false);
+		Level().spawn_item		(*name,position,level_vertex_id,id,false);
 	}
 
 	DEFICITS::const_iterator	I = m_deficits.find(name);
-	VERIFY3(I == m_deficits.end(), "Duplicate section in the purchase list", *name);
-	m_deficits.insert(
+	VERIFY3						(I == m_deficits.end(),"Duplicate section in the purchase list",*name);
+	m_deficits.insert			(
 		std::make_pair(
-		name,
-		(float)count*probability
-		/
-		_max((float)j, min_deficit_factor)
+			name,
+			(float)count*probability
+			/
+			_max((float)j,min_deficit_factor)
 		)
-		);
+	);
 }
