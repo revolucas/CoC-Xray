1815c0ad55bf588195baa77b11eb0b0d7cb898af
revolucas <revolucas@gmail.com>
2018-05-09 04:46:54 -0400

    Fix NPC reaction to car weapon by v2v3v4

--- src/xrGame/CarWeapon.cpp
+++ src/xrGame/CarWeapon.cpp
@@ -10,6 +10,8 @@
 #include "xr_level_controller.h"
 #include "game_object_space.h"
 
+#include "holder_custom.h"
+
 void CCarWeapon::BoneCallbackX		(CBoneInstance *B)
 {
 	CCarWeapon	*P = static_cast<CCarWeapon*>(B->callback_param());
@@ -225,8 +227,8 @@ void CCarWeapon::FireEnd()
 
 void CCarWeapon::OnShot()
 {
-	FireBullet				(	m_fire_pos, m_fire_dir, fireDispersionBase, *m_Ammo, 
-								m_object->ID(), m_object->ID(), SendHitAllowed(m_object), ::Random.randI(0,30));
+	CHolderCustom* holder = smart_cast<CHolderCustom*>(m_object);
+	FireBullet				(	m_fire_pos, m_fire_dir, fireDispersionBase, *m_Ammo, holder->Engaged() ? 0 : m_object->ID(), m_object->ID(), SendHitAllowed(m_object), ::Random.randI(0, 30));
 
 	StartShotParticles		();
 	
