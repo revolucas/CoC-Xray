4888fcf9d1db042b46fc8491cb946dbf7edbd2ea
revolucas <revolucas@gmail.com>
2018-06-14 00:18:31 -0400

    Remove usage of reduce_lod_texture_list from external.ltx and instead go by the console command value for all textures

--- src/Layers/xrRender/Texture.cpp
+++ src/Layers/xrRender/Texture.cpp
@@ -36,42 +36,12 @@ void fix_texture_name(LPSTR fn)
 
 int get_texture_load_lod(LPCSTR fn)
 {
-	CInifile::Sect& sect	= pSettings->r_section("reduce_lod_texture_list");
-	CInifile::SectCIt it_	= sect.Data.begin();
-	CInifile::SectCIt it_e_	= sect.Data.end();
-
-	CInifile::SectCIt it	= it_;
-	CInifile::SectCIt it_e	= it_e_;
-
 	static bool enough_address_space_available = is_enough_address_space_available();
 
-	for(;it!=it_e;++it)
-	{
-		if( strstr(fn, it->first.c_str()) )
-		{
-			if(psTextureLOD<1) {
-				if ( enough_address_space_available || (g_current_renderer < 2) )
-					return 0;
-				else
-					return 1;
-			}
-			else
-			if(psTextureLOD<3)
-				return 1;
-			else
-				return 2;
-		}
-	}
-
-	if(psTextureLOD<2) {
-//		if ( enough_address_space_available || (g_current_renderer < 2) )
-			return 0;
-//		else
-//			return 1;
-	}
-	else
-	if(psTextureLOD<4)
-		return 1;
+	if (g_current_renderer < 2)
+		return 0;
+	else if (enough_address_space_available)
+		return psTextureLOD % 2 == 0? psTextureLOD/2 : 0;
 	else
 		return 2;
 }
